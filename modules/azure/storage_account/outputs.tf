c9b16203d7e1bd4d3774d6ceec3eeee1768b9c04
mundadarithik <mundadarithik@gmail.com>
2026-03-14 10:04:16 +0530

    Create outputs.tf for storage account

---
+++ modules/azure/storage_account/outputs.tf
@@ -0,0 +1,19 @@
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
+
+output "storage_account_primary_blob_endpoint" {
+  value = azurerm_storage_account.example.primary_blob_endpoint
+}
+
+output "storage_account_primary_queue_endpoint" {
+  value = azurerm_storage_account.example.primary_queue_endpoint
+}
+
+output "storage_account_primary_table_endpoint" {
+  value = azurerm_storage_account.example.primary_table_endpoint
+}
+
+output "storage_account_primary_file_endpoint" {
+  value = azurerm_storage_account.example.primary_file_endpoint
+}
